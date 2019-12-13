8ef8f5b0fd8fcf97179a158aa278fd8013ed814a
Sara Mousavi <sara.mousavi.ch@gmail.com>
2019-12-13 22:03:05 +0000

    included prediction confidence

--- label_voting.py
+++ label_voting.py
@@ -11,9 +11,18 @@ df.columns = ('path', 'seq', 'label', 'conf')
 num_seq_uniq = df['seq'].unique().shape[0]
 
 for i in range(num_seq_uniq):
+
     labels = df.loc[df['seq']==df['seq'].unique()[i]]
     labels = labels.reset_index(drop = True)
+
+
     predicted_labels = list(labels['label'].values)
     counts = dict((x, predicted_labels.count(x)) for x in set(predicted_labels))
-    frequent_label = max(counts.items(), key = operator.itemgetter(1))[0]
-    print(labels['seq'].unique()[0], ":",  frequent_label)
+    frequent_label = max(counts.items(), key = operator.itemgetter(1))[0].strip()
+
+    confs = list(labels['conf'].values)
+    counts_conf = dict((x, confs.count(x)) for x in set(confs))
+    frequent_conf = max(counts_conf.items(), key = operator.itemgetter(1))[0]
+
+    
+    print(labels['seq'].unique()[0], ":",  frequent_label, ":", frequent_conf)
