d5e5d3f420e453f001cc0dd07fadd9cc6bae1f42
Sara Mousavi <sara.mousavi.ch@gmail.com>
2020-01-14 12:40:12 -0600

    current version of the classifier and predictor

--- classifier.py
+++ classifier.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+# run python3 classifier.py very_clean_label_data
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,11 +13,12 @@ import keras.backend as K
 from keras.utils import to_categorical
 import pickle
 from keras.applications import VGG16
+from keras.applications import ResNet50
 from keras import optimizers
 from sklearn.model_selection import train_test_split
 from keras.callbacks import EarlyStopping
 from keras.callbacks import ModelCheckpoint
-from keras.utils import multi_gpu_model
+#from keras.utils import multi_gpu_model
 
 from os import mkdir
 import glob
@@ -29,148 +30,109 @@ import sys
 import pickle
 
 
+train_data = sys.argv[1] 
+#model_names = ['resnet', 'vgg', 'inception'] 
+model_names = ['inception'] 
+
+import bpython
+bpython.embed(locals())
+
 VAL_SIZE = 0.30
 TEST_SIZE = 0.15
 
-body_parts = ['arm', 'hand', 'foot',  'legs','fullbody', 'head','butt', 'torso', 'stake', 'plastic']
+body_parts = ['arm', 'hand', 'foot',  'legs','fullbody', 
+        'head','backside', 'torso', 'stake', 'plastic']
 num_classes = len(body_parts)
 
 
 data = []
 labels = []
 
-clusters = sys.argv[1]#'clean_labeld_clusters'#
-#img_size = 299
-#299 is for inception model
-img_size = 224
+
+vgg_resnet_img_size = 224
+inception_img_size = 299
+
 batch_size = 32
 
-inp = Input((img_size, img_size, 3))
+for model_name in model_names:
 
-#inceptionV3_model = InceptionV3(include_top = False, weights='imagenet', 
-#                                input_tensor = inp, input_shape = (img_size, img_size, 3), pooling = 'avg')
-model = VGG16(include_top = False, weights='imagenet', 
-                                input_tensor = inp, input_shape = (img_size, img_size, 3), pooling = 'avg')
-x = model.output
-x = Dense(256, activation='relu')(x)
-x = Dropout(0.1)(x)
-out = Dense(num_classes, activation='softmax')(x)
+    if model_name == 'vgg':
+        inp = Input((vgg_resnet_img_size, vgg_resnet_img_size, 3))
+        model = VGG16(include_top = False, weights='imagenet', 
+                                        input_tensor = inp, input_shape = (vgg_resnet_img_size, vgg_resnet_img_size, 3),
+                                        pooling = 'avg')
 
-model = Model(inp, out)
-model.summary()
+    if model_name == 'resnet':
+        inp = Input((vgg_resnet_img_size, vgg_resnet_img_size, 3))
+        model = ResNet50(include_top = False, weights='imagenet', 
+                                        input_tensor = inp, input_shape = (vgg_resnet_img_size, vgg_resnet_img_size, 3),
+                                        pooling = 'avg')
+    if model_name == 'inception':
+        inp = Input((inception_img_size, inception_img_size, 3))
+        model = InceptionV3(include_top = False, weights='imagenet', 
+                                        input_tensor = inp, input_shape = (inception_img_size, inception_img_size, 3), 
+                                        pooling = 'avg')
 
-model = multi_gpu_model(model, gpus = 4)
+    x = model.output
+    x = Dense(256, activation='relu')(x)
+    x = Dropout(0.1)(x)
+    out = Dense(num_classes, activation='softmax')(x)
 
-sgd = optimizers.SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True, clipvalue = 0.5)
+    model = Model(inp, out)
 
-#inceptionV3_model.compile(optimizer = 'sgd', loss = 'categorical_crossentropy', metrics=['accuracy'])
-model.compile(optimizer = 'sgd', loss = 'categorical_crossentropy', metrics=['acc'])
+    #model = multi_gpu_model(model, gpus = 4)
 
-'''
-if os.path.isfile('data.pkl') and os.path.isfile('labels.pkl'):
-    with open('data.pkl', 'rb') as f:
-        data = pickle.load(f)
-    with open('labels.pkl', 'rb') as f:
-        labels = pickle.load(f)
-else:
-'''
-not_found = 0
-with open(clusters, 'r') as file_:
-    csv_reader = csv.reader(file_, delimiter = ":")
-    for row in csv_reader:
-        tag = row[1].strip()
-        if tag in body_parts:
-            try:
-                img = image.load_img(row[0].strip(), target_size = (img_size, img_size, 3), grayscale = False)
-                img = image.img_to_array(img)
-                img = img/255
-                data.append(img)
-                labels.append(body_parts.index(tag))
-                
+    sgd = optimizers.SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True, clipvalue = 0.5)
 
-            except:
-                not_found += 1
+    #inceptionV3_model.compile(optimizer = 'sgd', loss = 'categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer = 'sgd', loss = 'categorical_crossentropy', metrics=['acc'])
 
-data = np.array(data)
-labels = to_categorical(np.array(labels), num_classes = num_classes)
-'''
-try:
-    with open('data.pkl', 'wb') as f:
-        pickle.dump(data, f)
-    with open('labels.pkl', 'wb') as f:
-        pickle.dump(labels, f)
-except:
-    print("Could not write the data in files")
-'''
-#labels= np.array(labels)
+    not_found = 0
+    #if model_name != 'vgg': # becase for vgg the same data that was used for resnet will work
+    data = []
+    labels = []
 
-print("Done with reading in the data")
-print("Data size is {} and {}".format(data.shape, labels.shape))
-X_train, X_test, y_train, y_test = train_test_split(data, labels, random_state=42, test_size=0.3)
+    with open(train_data, 'r') as file_:
+        csv_reader = csv.reader(file_, delimiter = ":")
+        for row in csv_reader:
+            tag = row[1].strip()
+            if tag in body_parts:
+                try:
+                    if model_name == 'inception':
+                        img = image.load_img(row[0].strip(), 
+                                target_size = (inception_img_size, inception_img_size, 3), grayscale = False)
+                    else:
+                        img = image.load_img(row[0].strip(), 
+                                target_size = (vgg_resnet_img_size, vgg_resnet_img_size, 3), grayscale = False)
 
+                    img = image.img_to_array(img)
+                    img = img/255
+                    data.append(img)
+                    labels.append(body_parts.index(tag))
 
-#earlyStopping = EarlyStopping(monitor='val_acc', patience=10, verbose=0, mode='min')
+                except:
+                    not_found += 1
 
-checkpoint = ModelCheckpoint('test_model_epoch_-{epoch:03d}-_acc_{acc:03f}-_val_acc_{val_acc:.3f}.h5', verbose=1, monitor='val_acc', save_best_only=True, mode='auto')  
- 
+    data = np.array(data)
+    labels = to_categorical(np.array(labels), num_classes = num_classes)
 
-'''
-
-#data generator for train and val data
-train_datagen = ImageDataGenerator(rotation_range=10, width_shift_range=0.2,
-                                   height_shift_range=0.2, 
-                                   fill_mode='nearest', horizontal_flip=True,
-                                   vertical_flip=True, rescale=1./255, 
-                                   preprocessing_function=None)
-
-'''
-train_datagen = ImageDataGenerator()#rescale=1./255)
-val_datagen = ImageDataGenerator()#rescale=1./255)
-#train_datagen.fit(X_train)
-#val_datagen.fit(X_test)
+    X_train, X_test, y_train, y_test = train_test_split(data, labels, 
+             test_size=0.3)
 
 
 
-history = model.fit_generator(train_datagen.flow(X_train, y_train, batch_size=batch_size), 
-                                                    steps_per_epoch = len(X_train) // batch_size,
-                                                    validation_data=val_datagen.flow(X_test, y_test, batch_size=batch_size), 
-                                                    validation_steps=(len(X_test))//batch_size,
-                                                    callbacks=[checkpoint],
-                                                    epochs = 100, verbose = 1) 
+    checkpoint = ModelCheckpoint(model_name + '_5000_random_epoch_-{epoch:03d}-_acc_{acc:03f}-_val_acc_{val_acc:.5f}.h5', verbose=1, monitor='val_acc', save_best_only=True, mode='auto')  
+     
+    train_datagen = ImageDataGenerator()#rescale=1./255)
+    val_datagen = ImageDataGenerator()#rescale=1./255)
 
-'''
-history = model.fit(X_train, y_train, validation_data=(X_test, y_test),
-                    steps_per_epoch = len(X_train) // batch_size,
-                    validation_steps=(len(X_test)*0.2)//batch_size,
-                    callbacks=[checkpoint],
-                    epochs = 2, verbose = 1) 
 
-'''
-'''
-#history = inceptionV3_model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test))
-model.save('VGG16model_retrained_moredata.h5')
-with open('history', 'wb') as file_:
-    pickle.dump(history.history, file_)
+    history = model.fit_generator(train_datagen.flow(X_train, y_train, batch_size=batch_size), 
+                                                        steps_per_epoch = len(X_train) // batch_size,
+                                                        validation_data=val_datagen.flow(X_test,
+                                                            y_test, batch_size=batch_size), 
+                                                        validation_steps=(len(X_test))//batch_size,
+                                                        callbacks=[checkpoint],
+                                                        epochs = 100, verbose = 1) 
 
-fig1 = plt.figure()
-plt.plot(history.history['loss'],'r',linewidth=3.0)
-plt.plot(history.history['val_loss'],'b',linewidth=3.0)
-plt.legend(['Training loss', 'Validation Loss'],fontsize=18)
-plt.xlabel('Epochs ',fontsize=16)
-plt.ylabel('Loss',fontsize=16)
-plt.title('Loss Curves :CNN',fontsize=16)
-fig1.savefig('more_data_loss_cnn.png')
-plt.show()
-
-fig2=plt.figure()
-plt.plot(history.history['acc'],'r',linewidth=3.0)
-plt.plot(history.history['val_acc'],'b',linewidth=3.0)
-plt.legend(['Training Accuracy', 'Validation Accuracy'],fontsize=18)
-plt.xlabel('Epochs ',fontsize=16)
-plt.ylabel('Accuracy',fontsize=16)
-plt.title('Accuracy Curves : CNN',fontsize=16)
-fig2.savefig('more_data_accuracy_cnn.png')
-plt.show()
-
-'''
 
--- predict_bodypart.py
+++ predict_bodypart.py
@@ -1,75 +1,165 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
 
 ## run : python3 predict_bodypart.py file_name
-#file name is each line a path and the : alak for the label
-#!/usr/bin/env python
+#file name is each line a path and then : the grount truth label
 
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-from keras.preprocessing import image
-from keras.preprocessing.image import ImageDataGenerator
-from keras.applications.inception_v3 import InceptionV3
-from keras.layers import Dropout, Dense, Input
-from keras.models import Sequential
-from keras.models import Model
-import keras.backend as K
-from keras.utils import to_categorical
 from keras.models import load_model
-import pickle
-from keras import optimizers
-from sklearn.model_selection import train_test_split
-from keras.callbacks import EarlyStopping
-from keras.callbacks import ModelCheckpoint
-import os
 import csv
 import sys
+from keras.preprocessing import image
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 
-test_data = []
-test_labels = []
-img_names = []
-#img_size = 299
-img_size = 224
+
+inception_img_size = 299
+vgg_resnet_img_size = 224
 batch_size = 2
 
 test_csv = sys.argv[1]
 
-body_parts = ['arm', 'hand', 'foot', 'legs','fullbody', 'head','butt',  'torso', 'stake', 'plastic', 'alak']
-num_classes = len(body_parts)
+body_parts = ['arm', 'hand', 'foot', 'legs','fullbody', 'head','backside',  'torso', 'stake', 'plastic']
 
-model = load_model('/models/vgg_model_moredata_epoch_-007-_acc_0.982805-_val_acc_0.982.h5')
-model.summary()
+'''
+models = ['models/resnet_model_epoch_-042-_acc_0.999860-_val_acc_0.98716.h5',
+        'models/vgg_model_epoch_-006-_acc_0.860008-_val_acc_0.96641.h5', 
+        'models/inception_model_epoch_-131-_acc_0.999860-_val_acc_0.98848.h5']
+model_names = ['resnet', 'vgg', 'inception']
+## after self training and more data
+models = ['models/self_trained_resnet_model_epoch_-098-_acc_0.999346-_val_acc_0.98339.h5',
+        'models/self_trained_vgg_model_epoch_-091-_acc_0.999782-_val_acc_0.98039.h5', 
+        'models/self_trained_inception_model_epoch_-050-_acc_0.999565-_val_acc_0.98141.h5']
+models = ['models/inception_3000_epoch_-125-_acc_1.000000-_val_acc_0.97430.h5',
+        'models/inception_7000_epoch_-148-_acc_0.999791-_val_acc_0.97693.h5',
+        'models/inception_model_epoch_-131-_acc_0.999860-_val_acc_0.98848.h5',
+        'models/self_trained_inception_model_epoch_-050-_acc_0.999565-_val_acc_0.98141.h5',
+        'models/resnet_3000_epoch_-075-_acc_1.000000-_val_acc_0.95444.h5', 
+        'models/resnet_7000_epoch_-106-_acc_0.999791-_val_acc_0.96662.h5',
+        'models/resnet_model_epoch_-042-_acc_0.999860-_val_acc_0.98716.h5',
+        'models/self_trained_resnet_model_epoch_-098-_acc_0.999346-_val_acc_0.98339.h5',
+        'models/vgg_3000_epoch_-123-_acc_1.000000-_val_acc_0.95911.h5',
+        'models/vgg_7000_epoch_-083-_acc_0.999582-_val_acc_0.96809.h5',
+        'models/vgg_model_epoch_-006-_acc_0.860008-_val_acc_0.96641.h5',
+        'models/self_trained_vgg_model_epoch_-091-_acc_0.999782-_val_acc_0.98039.h5',
+        'models/inception_5000_random_epoch_-034-_acc_1.000000-_val_acc_0.97165.h5',
+        'models/inception_ut01-13d_gtplus_hand_epoch_-016-_acc_0.999721-_val_acc_0.98022.h5']
 
-not_found = 0
-with open(test_csv, 'r') as file_:
-    csv_reader = csv.reader(file_, delimiter = ":")
-    for row in csv_reader:
-        tag = row[1].strip()
-        if tag in body_parts:
-            try:
-                img = image.load_img(row[0].strip(), target_size = (img_size, img_size, 3), grayscale = False)
-                img = image.img_to_array(img)
-                img = img/255
-                img_names.append(row[0])
-                test_data.append(img)
-                test_labels.append(body_parts.index(tag))
-            except:
-                not_found += 1
+model_names = ['inception', 'inception', 'inception', 'inception', 'resnet', 'resnet','resnet','resnet', 'vgg','vgg','vgg','vgg', 'inception', 'inception'] 
+'''
+
+models = ['models/self_trained_resnet_model_epoch_-098-_acc_0.999346-_val_acc_0.98339.h5',
+        'models/vgg_3000_epoch_-123-_acc_1.000000-_val_acc_0.95911.h5',
+        'models/vgg_7000_epoch_-083-_acc_0.999582-_val_acc_0.96809.h5',
+        'models/vgg_model_epoch_-006-_acc_0.860008-_val_acc_0.96641.h5',
+        'models/self_trained_vgg_model_epoch_-091-_acc_0.999782-_val_acc_0.98039.h5',
+        'models/inception_5000_random_epoch_-034-_acc_1.000000-_val_acc_0.97165.h5',
+        'models/inception_ut01-13d_gtplus_hand_epoch_-016-_acc_0.999721-_val_acc_0.98022.h5']
+model_names = ['resnet', 'vgg','vgg','vgg','vgg', 'inception', 'inception']
+
+for index, m in enumerate(models):
+    model_name = model_names[index]
+    model = load_model(m)
+    test_data = []
+    img_names = []
+    #model.summary()
+
+    not_found = 0
+
+    df = pd.read_csv(test_csv, names = ['path', 'gt'], sep = ':')
+
+    for path in df['path']:
+        try:
+            if model_name == 'resnet' or model_name == 'vgg':
+                img = image.load_img(path.strip(), 
+                        target_size = (vgg_resnet_img_size, vgg_resnet_img_size, 3), 
+                        grayscale = False)
+            elif model_name == 'inception':
+                img = image.load_img(path.strip(), 
+                        target_size = (inception_img_size, inception_img_size, 3), 
+                        grayscale = False)
+            img = image.img_to_array(img)
+            img = img/255
+            test_data.append(img)
+            img_names.append(path)
+        except:
+            not_found += 1
+
+    test = np.array(test_data)
+    '''
+    prediction = model.predict(test)
+    pred_classes = prediction.argmax(axis=-1)
+    conf = prediction.max(axis=-1)
 
 
-test = np.array(test_data)
-l = np.array(test_labels)
+    print(m)
+    for i, label in enumerate(list(pred_classes)):
+        print(img_names[i]+":", body_parts[label], conf[i])
 
-#model = load_model('model_4donors_epoch_-080-_acc_1.000000-_val_acc_0.931818.h5')
+    '''
+    ####### top 1 ######
+    print("top 1:")
+    #print(m)
+    gt = list(df['gt'].values)
 
-prediction = model.predict(test)
-pred_classes = prediction.argmax(axis=-1)
-conf = prediction.max(axis=-1)
+    prediction = model.predict(test)
+    pred_classes = prediction.argmax(axis=-1)
 
 
-for i, label in enumerate(list(pred_classes)):
-    print(img_names[i]+":", body_parts[label], conf[label])
+    pred = list(pred_classes)
+    pred = [body_parts[x] for x in pred]
+    CM = confusion_matrix(gt, pred, labels= body_parts)
+    TP = CM.diagonal()
+    FN = np.sum(CM, axis = 1) - TP
+    FP = np.sum(CM, axis = 0) - TP
+    AP = TP/(TP + FP)
+    #print("AP: ", AP)
+    mAP = np.mean(AP)
+    #print("mAP: ", mAP)
 
+    recall = TP/(TP + FN)
+    #print("recall: ", recall)
+    mrecall = np.mean(recall)
+    #print("mrecall: ", mrecall)
+
+    #print(confusion_matrix(gt, pred, labels= body_parts))
+    #print(accuracy_score(gt, pred))
+
+    print("moedel: {}, mAP: {}, mrecall: {}, aac: {}".format(m, mAP, mrecall,
+                                                    accuracy_score(gt, pred)))
+    ###### top 3 ######
+    print("top k:")
+    #print(m)
+    pred = []
+    k = 3
+    for index, p in enumerate(prediction): #p is for each image
+        preds = p.argsort()[0-k:] # the top k confident predictions
+        added = False
+        for p in preds:
+            predicted = body_parts[p]
+            if predicted == gt[index]: # if one of the top k is the right prediction
+                pred.append(predicted)
+                added = True
+                break
+        if added == False:        
+            pred.append(body_parts[preds[-1]]) # the most confident
+
+    CM = confusion_matrix(gt, pred, labels= body_parts)
+    TP = CM.diagonal()
+    FN = np.sum(CM, axis = 1) - TP
+    FP = np.sum(CM, axis = 0) - TP
+    AP = TP/(TP + FP)
+    #print("AP: ", AP)
+    mAP = np.mean(AP)
+    #print("mAP: ", mAP)
+
+    recall = TP/(TP + FN)
+    #print("recall: ", recall)
+    mrecall = np.mean(recall)
+    #print("mrecall: ", mrecall)
+
+    #print(confusion_matrix(gt, pred, labels= body_parts))
+    #print(accuracy_score(gt, pred))
+    print("moedel: {}, mAP: {}, mrecall: {}, aac: {}".format(m, mAP, mrecall,
+                                                    accuracy_score(gt, pred)))
